91b4ea31819db67d453d725ac17802e70f06ef43
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-12 23:44:45 -0300

    password is now write-only

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,8 +1,19 @@
 from rest_framework import fields, serializers
 from apps.users.models import User
+from django.contrib.auth.hashers import make_password
 
 
 class SignInSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["email", "name", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        if validated_data.get("password"):
+            validated_data["password"] = make_password(validated_data["password"])
+
+        user = User(**validated_data)
+        user.save()
+
+        return user
