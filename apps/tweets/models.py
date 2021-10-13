bbfbf525dab880c6774738d983a2ba00224b7b6c
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-13 09:57:03 -0300

    Added the through field

--- apps/tweets/models.py
+++ apps/tweets/models.py
@@ -7,7 +7,9 @@ class Tweet(models.Model):
     parent = models.ForeignKey("Tweet", null=True, on_delete=models.SET_NULL)
     content = models.CharField(max_length=280)
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name="tweet_user", blank=True)
+    likes = models.ManyToManyField(
+        User, related_name="tweet_user", blank=True, through="TweetLike"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
@@ -15,3 +17,9 @@ class Tweet(models.Model):
     def __str__(self):
         content = self.content[0:50] + "..." if len(self.content) > 50 else self.content
         return f"{self.owner} | {content}"
+
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
