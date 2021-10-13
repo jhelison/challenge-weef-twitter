7f050de177e2751d74a8e6a899212bcfdf7fdff4
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-12 23:22:22 -0300

    Create the sign_in view

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import fields, serializers
 from apps.users.models import User
 
 
-class RegistrationSerializer(serializers.ModelSerializer):
+class SignInSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "name"]
+        fields = ["email", "name", "password"]
