a44c1d89c471b1b8d6eb99036f29b786e2e2af0e
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-13 00:20:36 -0300

    Added login

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
-from .views import sign_in
+from .views import sign_in, login
 
-urlpatterns = [path("sign-in/", sign_in)]
+urlpatterns = [path("sign-in/", sign_in), path("login/", login)]
