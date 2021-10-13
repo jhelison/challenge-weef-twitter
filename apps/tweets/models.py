5ca1a4676faf20d42acb9bb55865070a78751555
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-13 09:48:42 -0300

    Added the tweet model

--- apps/tweets/models.py
+++ apps/tweets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import User
+
+
+class Tweet(models.Model):
+    parent = models.ForeignKey("Tweet", null=True, on_delete=models.SET_NULL)
+    content = models.CharField(max_length=280)
+    owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name="tweet_user", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        content = self.content[0:50] + "..." if len(self.content) > 50 else self.content
+        return f"{self.owner} | {content}"
