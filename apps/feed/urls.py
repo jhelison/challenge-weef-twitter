8d73f393361e3f052e0f95c51395b645ad2e297d
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-13 21:41:55 -0300

    Added followers feed

--- apps/feed/urls.py
+++ apps/feed/urls.py
@@ -3,4 +3,5 @@ from apps.feed import views
 
 urlpatterns = [
     path("global", views.global_feed),
+    path("followers", views.followers_feed),
 ]
--- apps/feed/views.py
+++ apps/feed/views.py
@@ -1,12 +1,29 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from apps.tweets.models import Tweet
 from apps.tweets.serializers import TweetSerializer
+from apps.users.models import UserFollowing
 
 
 @api_view(["GET"])
 def global_feed(request):
     tweets = Tweet.objects.all()
     serializer = TweetSerializer(tweets, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def followers_feed(request):
+    following_ids = UserFollowing.objects.filter(
+        following_user_id=request.user
+    ).values_list("user_id")
+
+    print(following_ids)
+
+    tweets = Tweet.objects.filter(owner__in=following_ids)
+    serializer = TweetSerializer(tweets, many=True)
+
     return Response(serializer.data)
