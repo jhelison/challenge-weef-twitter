7877ef9c9e5a30d57e9e21a333b1b7ff20e30e70
Jhelison Uchoa <68653689+jhelison@users.noreply.github.com>
2021-10-14 11:00:45 -0300

    Cleanned urls

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.users import views
 
 urlpatterns = [
-    path("signin/", views.signin),
-    path("login/", views.login),
-    path("logout/", views.logout),
+    path("signin", views.signin),
+    path("login", views.login),
+    path("logout", views.logout),
 ]
